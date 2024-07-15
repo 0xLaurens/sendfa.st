05dd8928da3adb52f6ff37a7de4511224d980b30
0xlaurens <lauwiv@gmail.com>
2024-07-15 15:58:44 +0200

    server -> setup server wrapper

---
+++ backend/server/server.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"strconv"
+)
+
+type Server struct {
+	app  *fiber.App
+	port int
+}
+
+func NewServer(opts ...Options) *Server {
+	server := &Server{
+		app:  fiber.New(),
+		port: 7331,
+	}
+
+	for _, opt := range opts {
+		opt(server)
+	}
+
+	return server
+}
+
+func (s *Server) Run() error {
+	port := fmt.Sprintf(":%s", strconv.Itoa(s.port))
+	log.Printf("Server running on port %s", port)
+	return s.app.Listen(port)
+}
+
+func (s *Server) Shutdown() error {
+	return s.app.Shutdown()
+}
