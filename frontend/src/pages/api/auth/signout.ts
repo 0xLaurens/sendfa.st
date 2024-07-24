56aedd1ac6006767d6a2dc30e1f74e44aa7af200
0xlaurens <lauwiv@gmail.com>
2024-07-24 13:38:38 +0200

    supabase -> email and password login

---
+++ frontend/src/pages/api/auth/signout.ts
@@ -0,0 +1,9 @@
+// With `output: 'hybrid'` configured:
+export const prerender = false;
+import type {APIRoute} from "astro";
+
+export const GET: APIRoute = async ({cookies, redirect}) => {
+    cookies.delete("sb-access-token", {path: "/"});
+    cookies.delete("sb-refresh-token", {path: "/"});
+    return redirect("/signin");
+}
