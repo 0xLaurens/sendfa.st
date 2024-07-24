da75f1e296956cee914053cee5c2d86995f23b0e
0xlaurens <lauwiv@gmail.com>
2024-07-24 14:03:51 +0200

    auth -> broken signout fixed

--- frontend/src/pages/api/auth/signout.ts
+++ frontend/src/pages/api/auth/signout.ts
@@ -2,7 +2,7 @@
 export const prerender = false;
 import type {APIRoute} from "astro";
 
-export const GET: APIRoute = async ({cookies, redirect}) => {
+export const POST: APIRoute = async ({cookies, redirect}) => {
     cookies.delete("sb-access-token", {path: "/"});
     cookies.delete("sb-refresh-token", {path: "/"});
     return redirect("/signin");
