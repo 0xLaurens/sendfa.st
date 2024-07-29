0ff4d338ec863c7bf3c0354e94051a625e2f8627
0xlaurens <lauwiv@gmail.com>
2024-07-29 17:32:50 +0200

    deployment -> cloudflare deployment changes

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
-
+import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  integrations: [tailwind()]
+    output: "hybrid",
+    integrations: [tailwind()],
+    adapter: cloudflare({
+        imageService: "compile",
+    }),
 });
