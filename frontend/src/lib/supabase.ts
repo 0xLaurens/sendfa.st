fc897956177e744f9a0ae9fb1e5cb2afe2cf18cd
0xlaurens <lauwiv@gmail.com>
2024-07-29 11:54:23 +0200

    navbar -> accesibility (alt for logo)

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,6 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
+import {createClient} from "@supabase/supabase-js";
 
 export const supabase = createClient(
     import.meta.env.SUPABASE_URL,
     import.meta.env.SUPABASE_ANON_KEY,
+    {
+        auth: {
+            flowType: "pkce"
+        }
+    }
 );
