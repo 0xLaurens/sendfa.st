826a4f5bd607bc01a657bb3785c837f12bcc04c5
0xlaurens <lauwiv@gmail.com>
2024-08-09 11:23:23 +0200

    stripe -> buy buttons

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,8 +1,11 @@
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_ANON_KEY: string
+    readonly SUPABASE_URL: string
+    readonly SUPABASE_ANON_KEY: string
+    readonly STRIPE_PUBLIC_KEY: string
+    readonly STRIPE_KEY: string
+    readonly SENDFAST_PRO_URL: string
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv
 }
