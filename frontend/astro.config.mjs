83a7e4e6356e3943d867edfcf38a0aff455b839c
0xlaurens <lauwiv@gmail.com>
2024-08-09 13:42:40 +0200

    svelte -> init

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,17 +1,16 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://sendfa.st",
-    output: "hybrid",
-    integrations: [
-        sitemap(),
-        tailwind()
-    ],
-    adapter: cloudflare({
-        imageService: "compile",
-    }),
+  site: "https://sendfa.st",
+  output: "hybrid",
+  integrations: [sitemap(), tailwind(), svelte()],
+  adapter: cloudflare({
+    imageService: "compile"
+  })
 });
