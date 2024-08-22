f2a68ae5c661e62140da24f1a170d8191d796972
0xlaurens <lauwiv@gmail.com>
2024-08-22 10:27:57 +0200

    toast centered with padding for small screens

--- frontend/src/components/Toaster.svelte
+++ frontend/src/components/Toaster.svelte
@@ -15,33 +15,35 @@
     import ErrorIcon from "./ErrorIcon.svelte";
 </script>
 
-<div class="fixed left-0 right-0 top-4 mx-auto flex flex-col gap-4 z-50 w-max h-max"
-     role="alert"
-     aria-live="assertive"
-     aria-atomic="true"
->
-    {#each $toasts as toast (toast.id)}
-        <div class="card card-compact card-bordered z-10 bg-base-100 shadow-xl w-96 transition-popup">
-            <div class="card-body flex flex-row">
-                <div class="my-auto px-2">
-                    {#if toast.type === "success"}
-                        <CheckmarkIcon/>
-                    {/if}
-                    {#if toast.type === "error"}
-                        <ErrorIcon/>
-                    {/if}
-                </div>
-                <div>
+<div class="flex justify-center items-center">
+    <div class="fixed top-0 left-0 right-0 flex justify-center items-start p-4 z-50"
+         role="alert"
+         aria-live="assertive"
+         aria-atomic="true"
+    >
+        {#each $toasts as toast (toast.id)}
+            <div class="card card-compact card-bordered z-10 bg-base-100 shadow-xl w-96 transition-popup">
+                <div class="card-body flex flex-row">
+                    <div class="my-auto px-2">
+                        {#if toast.type === "success"}
+                            <CheckmarkIcon/>
+                        {/if}
+                        {#if toast.type === "error"}
+                            <ErrorIcon/>
+                        {/if}
+                    </div>
                     <div>
-                        <h3 class="font-bold text-lg">
-                            {toast.title}
-                        </h3>
                         <div>
-                            {toast.description}
+                            <h3 class="font-bold text-lg">
+                                {toast.title}
+                            </h3>
+                            <div>
+                                {toast.description}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    {/each}
+        {/each}
+    </div>
 </div>
