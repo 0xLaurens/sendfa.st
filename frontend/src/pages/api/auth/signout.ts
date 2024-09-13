04d6958cc43b6c6f63b2b7601254fa1e9b07a269
0xlaurens <lauwiv@gmail.com>
2024-09-13 15:32:26 +0200

    auth removed

--- frontend/src/pages/api/auth/signout.ts
+++
@@ -1,9 +0,0 @@
-// With `output: 'hybrid'` configured:
-export const prerender = false;
-import type {APIRoute} from "astro";
-
-export const POST: APIRoute = async ({cookies, redirect}) => {
-    cookies.delete("sb-access-token", {path: "/"});
-    cookies.delete("sb-refresh-token", {path: "/"});
-    return redirect("/signin");
-}
