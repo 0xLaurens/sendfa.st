fbb511958bb3c4122d400ff10038414c60e53ce5
0xlaurens <lauwiv@gmail.com>
2024-09-28 10:45:08 +0200

    truncate name handle undefined strings

--- frontend/src/util/truncate.ts
+++ frontend/src/util/truncate.ts
@@ -1,4 +1,7 @@
-export function truncateFileName(fileName: string): string {
+export function truncateFileName(fileName: string | undefined): string {
+    if (!fileName) {
+        return '';
+    }
     let extension = fileExtension(fileName);
     if (fileName.length > 25) {
         return fileName.slice(0, 25) + '...' + extension;
