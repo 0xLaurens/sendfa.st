48ef7e68241bda4777bd39e7ccb8d72572da1e77
0xlaurens <lauwiv@gmail.com>
2024-10-05 11:44:24 +0200

    environment variables updated to be accessible client side

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,13 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 interface ImportMetaEnv {
-    readonly SUPABASE_URL: string
-    readonly SUPABASE_ANON_KEY: string
-    readonly STRIPE_PUBLIC_KEY: string
-    readonly STRIPE_KEY: string
-    readonly SENDFAST_PRO_URL: string
-    readonly SUPPORT_EMAIL: string
-    readonly WS_PROTOCOL: string
-    readonly WS_HOST: string
+    readonly PUBLIC_SUPPORT_EMAIL: string
+    readonly PUBLIC_WS_PROTOCOL: string
+    readonly PUBLIC_WS_HOST: string
 }
 
 interface ImportMeta {
