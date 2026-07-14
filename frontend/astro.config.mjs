0cabab87fcd4cd2aed968b42c6802924913997bd
Laurens Weterings <lauwiv@gmail.com>
2026-07-14 22:12:10 +0200

    fix: make it server instead of static

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -8,7 +8,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
     site: "https://sendfa.st",
-    output: "static",
+    output: "server",
     integrations: [sitemap(), svelte()],
     adapter: cloudflare({
         imageService: "cloudflare"
